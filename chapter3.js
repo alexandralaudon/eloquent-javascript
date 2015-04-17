ce858ed80aa334a1224e7d22861c63b3b7d98b0b
Alex <alexandralaudon@gmail.com>
2015-04-17 15:18:10 -0600

    Completed 3.2 Recursion

--- chapter3.js
+++ chapter3.js
@@ -3,4 +3,10 @@
     if (num1 === num2) {return NaN;}
     return (num1 > num2) ? num1 : num2;
   }
-  
+
+//3.2 Recursion:
+function isEven(num) {
+  if (num < 0) {return "??";}
+  if (num < 2) {return (num === 0);}
+  return isEven(num-2);
+}
