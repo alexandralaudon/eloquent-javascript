86b8cafcd131bcae20f1a527d99e04be042d9406
Alex <alexandralaudon@gmail.com>
2015-04-17 15:15:22 -0600

    Completed 2.2: FizzBuzz

--- chapter2.js
+++ chapter2.js
@@ -1,5 +1,17 @@
 //2.1:Looping a triangle
-	word = ''
-  for (var i = 0; i < 7; i++) {
-	  console.log(word+='#');
+	var tri_loop = function() {
+    word = ''
+    for (var i = 0; i < 7; i++) {
+  	  console.log(word+='#');
+    }
+  }
+
+//2.2: FizzBuzz
+  var fizzBuzz = function(){
+  	for (var i = 1; i <= 100; i++) {
+  		var word = '';
+  		if (i%3 === 0) { word += 'Fizz';}
+  		if (i%5 === 0) { word += 'Buzz';}
+  		console.log(word || i);
+  	}
   }
