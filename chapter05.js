0000a98f41f7052f58f47f493df9857b8a222922
Alex <alexandralaudon@gmail.com>
2015-04-19 12:59:39 -0600

    Completed 5.2 Mother-Child Age Difference

--- chapter05.js
+++ chapter05.js
@@ -4,3 +4,31 @@
   console.log(arrays.reduce(function(all, next){
   	return all.concat(next);
   }));
+
+//5.2 Mother-Child Age Difference
+  function average(array) {
+    function plus(a, b) { return a + b; }
+    return array.reduce(plus) / array.length;
+  }
+
+  var byName = {};
+  ancestry.forEach(function(person) {
+    byName[person.name] = person;
+  });
+
+  // Your code here.
+  function ageDifference(person){
+    if (byName[person.mother] == undefined) {return undefined;}
+    return Math.abs(person.born - byName[person.mother].born);
+  }
+
+  function motherAgeArray(){
+    var ageArray = [];
+    for (var i = 0; i < ancestry.length; i++){
+       var short_term = ageDifference(ancestry[i]);
+       if (short_term !== undefined) {ageArray.push(short_term);}
+    }
+    return average(ageArray);
+  }
+
+  console.log(motherAgeArray());
