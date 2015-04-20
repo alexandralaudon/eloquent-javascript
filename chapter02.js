fb18ad1c0110c913f02c5f04858388ad8ed5b0c1
Alex <alexandralaudon@gmail.com>
2015-04-20 08:44:25 -0600

    Updated 02 to reflect JsBin

--- chapter02.js
+++ chapter02.js
@@ -1,27 +1,25 @@
 //2.1:Looping a triangle
-	var triangle_loop = function() {
-    word = ''
-    for (var i = 0; i < 7; i++) {
-  	  console.log(word+='#');
-    }
+  var limit = 8;
+  for (var word = "#"; limit > word.length ; word += "#") {
+	  console.log(word);
   }
 
 //2.2: FizzBuzz
-  var fizzBuzz = function(){
-  	for (var i = 1; i <= 100; i++) {
-  		var word = '';
-  		if (i%3 === 0) { word += 'Fizz';}
-  		if (i%5 === 0) { word += 'Buzz';}
-  		console.log(word || i);
-  	}
-  }
-
+	for (var i = 1; i <= 100; i++) {
+		var word = '';
+		if (i%3 === 0) { word += 'Fizz';}
+		if (i%5 === 0) { word += 'Buzz';}
+		console.log(word || i);
+	}
 //2.3 Chessboard
-	var chessboard = function() {
-    var size = 8;
-  	for (var i = 0; i < size; i++) {
-  	     row = '# '.repeat(size)
-  	     if (i%2 === 0) {console.log(' ' + row);}
-  	     else console.log(row);
-  	}
-  }
+	var chessboard = "";
+	var size = 8;
+	for (var x = 0; x < size; x++){
+	  for (var y = 0; y < size; y++) {
+	    if ((x+y)%2===0){chessboard += " ";}
+	    else {chessboard += "#";}
+	  }
+	  chessboard += "\n";
+	}
+
+	console.log(chessboard);
