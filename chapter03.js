8f8c11b985c8338d629aae69fe306b5ea5ebd9f1
Alex <alexandralaudon@gmail.com>
2015-04-20 12:38:43 -0600

    Updated 03 with console.log messages

--- chapter03.js
+++ chapter03.js
@@ -1,28 +1,37 @@
 //3.1 Minimum:
   function min(num1, num2) {
     if (num1 === num2) {return NaN;}
-    return (num1 > num2) ? num1 : num2;
+    return (num1 > num2) ? num2 : num1;
   }
+
+  console.log(min(0, 10));
+  console.log(min(0, -10));
 
 //3.2 Recursion:
-function isEven(num) {
-  if (num < 0) {return "??";}
-  if (num < 2) {return (num === 0);}
-  return isEven(num-2);
-}
+  function isEven(num) {
+    if (num < 0) {return "??";}
+    if (num < 2) {return (num === 0);}
+    return isEven(num-2);
+  }
+
+  console.log(isEven(50));
+  console.log(isEven(75));
+  console.log(isEven(-1));
 
 //3.3 Bean Counting:
-var letter = 'B';
-
-function countBs(str) {
-  count = 0;
-  for (var i=0 ; i < str.length ; i++) {
-    if (str.charAt(i) === letter) {count++;}
+  function countBs(str) {
+    count = 0;
+    for (var i=0 ; i < str.length ; i++) {
+      if (str.charAt(i) === letter) {count++;}
+    }
+    return count;
   }
-  return count;
-}
 
-function countChar(str,input_letter) {
-  letter = input_letter;
-  return countBs(str);
-}
+  function countChar(str,input_letter) {
+    letter = input_letter;
+    return countBs(str);
+  }
+
+  var letter = 'B';
+  console.log(countBs("BBC"));
+  console.log(countChar("kakkerlak", "k"));
