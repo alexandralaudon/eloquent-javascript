73ac5b35172d80ed00b672caa6fcf59c58b05a6f
Alex <alexandralaudon@gmail.com>
2015-04-21 09:51:08 -0600

    indented FizzBuzz properly

--- chapter02.js
+++ chapter02.js
@@ -7,8 +7,12 @@
 //2.2: FizzBuzz
 	for (var i = 1; i <= 100; i++) {
 		var word = '';
-		if (i%3 === 0) { word += 'Fizz';}
-		if (i%5 === 0) { word += 'Buzz';}
+		if (i%3 === 0) {
+      word += 'Fizz';
+    }
+		if (i%5 === 0) {
+      word += 'Buzz';
+    }
 		console.log(word || i);
 	}
 //2.3 Chessboard
